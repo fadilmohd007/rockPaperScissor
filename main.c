aa7255a961a0c70d98d9c315fa5bafd891f4d621
fadilmohd007 <fadilmohd007@gmail.com>
2024-12-22 12:25:02 +0100

    clean up

--- main.c
+++ main.c
@@ -19,16 +19,17 @@ int lookUp[3][3] =
     1,0,1,
     -1,1,0
 };
-int getGamePoint (enum RPS_CHOICE uchoice_i, enum RPS_CHOICE mchoice_i)
+int getGamePoint (enum RPS_CHOICE uChoiceI, enum RPS_CHOICE mChoiceI)
 {
-    return lookUp[uchoice_i][mchoice_i];
+    return lookUp[uChoiceI][mChoiceI];
 }
 
 int main(int argc, char** argv){
-    char uname[10];
-    char choice_c;
-    int uchoice_i,mchoice_i;//user choice and machine choice
+    char uName[10];
+    char choiceC;
+    int uChoiceI,mChoiceI;//user choice and machine choice
     int ret, gamePoint;
+    int userScore = 0;
     sqlite3 *db;
     char* dbName = "local.db";
     ret = CreateDB(dbName,&db);
@@ -47,30 +48,30 @@ int main(int argc, char** argv){
     }
     scan:
     printf("Enter user name: ");
-    // uname can be max 10 char
-    ret = scanf("%10s",uname);
+    // uName can be max 10 char
+    ret = scanf("%10s",uName);
     if(ret != 1)
     {
         printf( "Error in reading srting\n");
         goto scan;
     }
     char insert_sql[256];
-    snprintf(insert_sql, sizeof(insert_sql), "INSERT OR IGNORE INTO Users (Name, Score) VALUES ('%s', 0);", uname);
+    snprintf(insert_sql, sizeof(insert_sql), "INSERT OR IGNORE INTO Users (Name, Score) VALUES ('%s', 0);", uName);
     ret = DBExec(insert_sql, db);
     if(ret != 0)
     {
         printf("error inserting User in  db, exiting..\n");
         return -1;
     }
-    printf("welcome to Rock Paper Sciccor %s\n",uname);
-    int user_score = 0;
-    ret = FetchScore(uname, db, &user_score);
+    printf("welcome to Rock Paper Sciccor %s\n",uName);
+
+    ret = FetchScore(uName, db, &userScore);
     if(ret != 0)
     {
         printf("error fetching score, exiting..\n");
         return -1;
     }
-    printf("Your current score is %d\n",user_score);
+    printf("Your current score is %d\n",userScore);
 
     /*
     PLAY GAME
@@ -81,21 +82,21 @@ int main(int argc, char** argv){
             3/'c' for  scissor\n");
     scan_choice:
     printf("Choose your option:");
-    scanf(" %1c",&choice_c);
+    scanf(" %1c",&choiceC);
     while (getchar() != '\n');
-    uchoice_i = atoi(&choice_c);
-    if(uchoice_i == 1 || uchoice_i == 2 || uchoice_i == 3)
+    uChoiceI = atoi(&choiceC);
+    if(uChoiceI == 1 || uChoiceI == 2 || uChoiceI == 3)
         goto no_char_conv;
-    switch(choice_c)
+    switch(choiceC)
     {
         case('s'):
-            uchoice_i = stone;
+            uChoiceI = stone;
             break;
         case('p'):
-            uchoice_i = paper;
+            uChoiceI = paper;
             break;
         case('c'):
-            uchoice_i = scissor;
+            uChoiceI = scissor;
             break;
         default:
             printf("please read intruction carefully\n");
@@ -103,16 +104,16 @@ int main(int argc, char** argv){
     }
 
     no_char_conv:
-    printf("you entered option %s\n", choices[uchoice_i]);
-    mchoice_i = genIntRandomInRange(3,1);
-    printf("computer's option %d %s\n",mchoice_i, choices[mchoice_i]);
+    printf("you entered option %s\n", choices[uChoiceI]);
+    mChoiceI = genIntRandomInRange(3,1);
+    printf("computer's option %d %s\n",mChoiceI, choices[mChoiceI]);
     /*
     UPDATE SCORE
     */
-    gamePoint = getGamePoint(uchoice_i,mchoice_i);
+    gamePoint = getGamePoint(uChoiceI,mChoiceI);
     printf("You got  %d point\n", gamePoint);
-    user_score = user_score + gamePoint;
-    ret = UpdateScore( uname, db, user_score);
+    userScore = userScore + gamePoint;
+    ret = UpdateScore( uName, db, userScore);
     if(ret != 0)
     {
         printf("error inserting User in  db, exiting..\n");
